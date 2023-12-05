2d820ba1c6bab127e8b4dd7b7ed7f52e9dc8d515
João Vitor <21.01075-7@maua.br>
2023-12-05 20:01:09 -0300

    creating repeating function to interact with graph

--- src/pages/GraphPage/index.jsx
+++ src/pages/GraphPage/index.jsx
@@ -1,12 +1,19 @@
 import { useLocation } from "react-router-dom";
+import { useState } from "react";
 
 
 function GraphPage(){
     const location = useLocation();
-    
-    return(
+    const [value, setValue] = useState(0);
+    setInterval(() => {
+        setValue(value + 1);
+    }, 1000);
 
-        <p>Salve salve, o valor é {location.state.coef1}, {location.state.coef2}, {location.state.coef3} e {location.state.typeOfFormula}</p>
+    return (
+        <div>
+            <p>{value}</p>
+            <p>Salve salve, o valor é {location.state.coef1}, {location.state.coef2}, {location.state.coef3} e {location.state.typeOfFormula}</p>
+        </div>        
     );
 }
 
